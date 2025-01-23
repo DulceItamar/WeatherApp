866be522b4a63ab259dbf4208c2062937e394957
dulceitamar <dulceitamar22@gmail.com>
2025-01-23 16:13:37 -0600

    CHORE: Added config file and .gitignore to ignore it

--- WeatherApp/Service/APIClient/APIEndpoint.swift
+++ WeatherApp/Service/APIClient/APIEndpoint.swift
@@ -18,8 +18,34 @@ protocol EndPoint {
 
 struct APIEndpoint {
     var coordinates: Coordinates
+    var apiKey: String
     var method: HTTPMethod
+    var queries: [URLQueryItem]
     
+    static let baseURL = "https://api.openweathermap.org/data/2.5/forecast"
+ 
+    
+    
+    func getEndpoint() -> URL {
+        
+        let latitude = String(describing: coordinates.latitude)
+        let longitude = String(describing: coordinates.longitude)
+//        let apiKey: String = try Configuration.value(for: "API_KEY")
+        
+        var lastEndpoint : URL {
+            let endpoint = URL(string: APIEndpoint.baseURL)!
+                .appending(queryItems: [
+                    URLQueryItem(name: "lat", value: latitude),
+                    URLQueryItem(name: "long", value: longitude),
+                    URLQueryItem(name: "appid", value: apiKey),
+            ])
+            
+            print(endpoint)
+            return endpoint
+        }
+        
+        return lastEndpoint
+    }
     
 }
 
---
+++ WeatherApp/Service/APIClient/Parser.swift
@@ -0,0 +1,12 @@
+//
+//  Parser.swift
+//  WeatherApp
+//
+//  Created by Dulce Itamar Vigueras Ballesteros on 23/01/25.
+//
+
+import Foundation
+
+class Parser {
+    
+}
