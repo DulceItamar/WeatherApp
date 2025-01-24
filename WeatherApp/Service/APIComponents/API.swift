ab308423882ffe7d7dc0e333a958e0619e837805
dulceitamar <dulceitamar22@gmail.com>
2025-01-24 01:35:34 -0600

    Feat: Creation of the API Manager

---
+++ WeatherApp/Service/APIComponents/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  WeatherApp
+//
+//  Created by Dulce Itamar Vigueras Ballesteros on 23/01/25.
+//
+
+import Foundation
+
+class APIWeather {
+    
+    
+    let router = Router(appEnviroment: .baseURL)
+}
